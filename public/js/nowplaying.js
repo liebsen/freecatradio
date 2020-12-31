b7e23c2c0b4ecd77dcf481ed09a4eb98a0a0ff9c
liebsen <overlemonsoft@gmail.com>
2020-12-31 09:53:37 -0300

    build

--- public/js/nowplaying.js
+++ public/js/nowplaying.js
@@ -3,15 +3,25 @@ let currentSong = null
 nowPlaying = () => {
   const nowplaying = document.querySelector('.nowplaying')
   const headphones = '<span class="mdi mdi-headphones"></span> '
+  const microphone = '<span class="mdi mdi-microphone-variant"></span> '
+  let icon = ''
+  let currentTitle = ''
   axios.get('https://icecast.freecatradio.com/status-json.xsl').then(res => {
-    let title = res.data.icestats.source.title
-    if (currentSong !== title) {
+    let sources = res.data.icestats.source
+    if (sources[1] && sources[1].title) {
+      icon = microphone
+      currentTitle = sources[1].title
+    } else {
+      icon = headphones
+      currentTitle = sources[0].title
+    }
+    if (currentSong !== currentTitle) {
       nowplaying.classList.remove('fadeInUp', 'fadeOutUp')
       nowplaying.classList.add('fadeOutUp')
-      currentSong = title
+      currentSong = currentTitle
       setTimeout(() => {
         nowplaying.classList.remove('fadeInUp', 'fadeOutUp')
-        nowplaying.innerHTML = headphones + decodeURIComponent(title)
+        nowplaying.innerHTML = icon + decodeURIComponent(currentTitle)
         nowplaying.classList.add('fadeInUp')
       }, 1000)
     }
